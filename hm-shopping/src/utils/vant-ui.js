9f4bcd668cb548ad023fa479d249bb63caa90b48
ltpbje <19836788252@163.com>
2023-11-23 12:56:51 +0800

    120-首页-静态结构准备完成

--- hm-shopping/src/utils/vant-ui.js
+++ hm-shopping/src/utils/vant-ui.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 // 自动按需引入vant组件
-import { Toast, Button, Switch, Tabbar, TabbarItem, NavBar } from 'vant'
-
+import { Search, Swipe, SwipeItem, Grid, GridItem, Toast, Button, Switch, Tabbar, TabbarItem, NavBar } from 'vant'
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
 Vue.use(NavBar)
 Vue.use(Toast)
 Vue.use(Tabbar)
