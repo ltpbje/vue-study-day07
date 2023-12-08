658ee46d4111dc6a2b9c6d87f4d44d0bf11bb9a8
ltpbje <19836788252@163.com>
2023-12-08 09:36:14 +0800

    130-购物车-封装getters:动态计算展示完成

--- hm-shopping/src/store/modules/cart.js
+++ hm-shopping/src/store/modules/cart.js
@@ -6,6 +6,30 @@ export default {
       cartList: []
     }
   },
+  getters: {
+    // 商品总数
+    cartTotal (state) {
+      return state.cartList.reduce((sum, item) => {
+        return sum + item.goods_num
+      }, 0)
+    },
+    // 选中的商品列表
+    selCartList (state) {
+      return state.cartList.filter(item => item.isChecked)
+    },
+    // 选中的商品总数
+    selCount (state, getters) {
+      return getters.selCartList.reduce((sum, item) => {
+        return sum + item.goods_num
+      }, 0)
+    },
+    selPrice (state, getters) {
+      return getters.selCartList.reduce((sum, item) => {
+        return sum + item.goods_num * item.goods.goods_price_min
+      }, 0)
+    }
+
+  },
   mutations: {
     setCartList (state, newCartList) {
       state.cartList = newCartList
@@ -17,8 +41,9 @@ export default {
       // 后台返回的数据中，不包含复选框的选中状态，为了实现将来的功能
       // 需要手动维护数据，给每一项，添加一个isChecked状态（标记当前商品是否选中）
       data.list.forEach(item => {
-        item.isChecked = false
+        item.isChecked = true
       })
+      console.log(1, data)
       context.commit('setCartList', data.list)
     }
   }
