2cf96576ec6094764a474504638a5d6a38059d9e
ltpbje <19836788252@163.com>
2023-12-22 09:34:13 +0800

    ref和reactive的使用

---
+++ Day11-vue3-Learn to get started/vue3-demo/src/01-setup.vue
@@ -0,0 +1,30 @@
+<script setup>
+// export default {
+//   setup(){
+//     const message = 'this is a message'
+//     const logMessage  = ()=>{
+//         console.log(message)
+//     }
+//     return {
+//       message,
+//       logMessage
+//     }
+//   }
+// }
+const message = 'this is a message'
+const logMessage = () => {
+    console.log(message);
+}
+</script>
+
+
+
+<template>
+    <div>
+        <div>{{ message }}</div>
+        <button @click="logMessage">按钮</button>
+    </div>
+</template>
+
+
+<style></style>
--- Day11-vue3-Learn to get started/vue3-demo/src/App.vue
+++ Day11-vue3-Learn to get started/vue3-demo/src/App.vue
@@ -1,28 +1,29 @@
 <script setup>
-// export default {
-//   setup(){
-//     const message = 'this is a message'
-//     const logMessage  = ()=>{
-//         console.log(message)
-//     }
-//     return {
-//       message,
-//       logMessage
-//     }
-//   }
-// }
-const message = 'this is a message'
-const logMessage=()=>{
-  console.log(message);
-}
-</script>
+import { computed, ref } from 'vue'
 
+const list = ref([1,2,3,4,5,6,7])
+const filterList= computed(()=>{
+  return list.value.filter(item =>item > 2)
+})
+const addFn=()=>{
+  list.value.push(666)
+}
+// const val =ref(555)
+// const modifyList = computed({
+//   set:()=> list.value.filter(item => item > 2),
+//   get:(val.value) => {
+//     list.value.push(val.value) 
+//   }
+// })
+</script>
 
 
 <template>
   <div>
-    <div>{{ message }}</div>
-    <button @click="logMessage">按钮</button>
+    <div>原始的list {{ list }}</div>
+    <div>计算属性过滤后的list {{ filterList }}</div>
+    <button @click="addFn">按钮</button>
+    <button @click="addFn">按钮</button>
   </div>
 </template>
 
