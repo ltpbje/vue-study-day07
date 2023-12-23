c05c440c091c6f974cffa0e5462e13e932cb6019
ltpbje <19836788252@163.com>
2023-12-23 20:47:18 +0800

    组合式API-父子通信完成

--- Day11-vue3-Learn to get started/vue3-demo/src/App.vue
+++ Day11-vue3-Learn to get started/vue3-demo/src/App.vue
@@ -1,45 +1,37 @@
 <script setup>
-import { onMounted } from 'vue'
-
-// 生命周期函数
-//beforeCreate和created的相关代码 
-//一律放在setup中执行
-  const getList =()=>{
-    //设置一个延迟1秒的定时器
-    setTimeout(()=>{
-        //打印出获取数据的信息
-        console.log('发送请求,获取了数据');
-    } ,1000)
-   }
-  //调用getList函数
-  // 一进入页面的请求
-  getList()
-
-  // 如果有些代码需要在mounted生命周期中执行
- onMounted(()=>{
-    console.log('mounted生命周期函数 逻辑一');
- })
- onMounted(()=>{
-    console.log('mounted生命周期函数 逻辑二');
- })
-// 写成函数的调用方式，可以调用多次
-
-
+// 父传子
+import { ref } from 'vue'
+import sonCom from '@/components/son-com.vue'
+const money= ref(1000)
+const getMoney=()=>{
+  money.value+=200
+}
+const changeMoney = (newMoney)=>{
+  money.value = newMoney
+}
 </script>
 
 
 <template>
   <div>
-    <div>{{ count }}</div>
-    <button @click="changeCount">修改count</button>
-    <div>{{ name }}</div>
-    <button @click="changeName">修改name</button>
-    <div>{{ userInfo }}</div>
-    <button @click="changeUserInfo">修改userInfo</button>
+    <div class="father">
+      父组件
+      <button @click="getMoney">挣钱</button>
+    </div>
+    <sonCom 
+    car="梅赛德斯奔驰" 
+    :money="money"
+    @changeMoney="changeMoney"
+    ></sonCom>
   </div>
 </template>
 
 
 <style>
-
+/* 为类名为father的元素添加一个正常大小的边框 */
+.father {
+  width: 100px;
+  height: 50px;
+  color: white;
+}
 </style>
---
+++ Day11-vue3-Learn to get started/vue3-demo/src/components/son-com.vue
@@ -0,0 +1,32 @@
+<script setup>
+//注意：由于写了setup,所以无法直接配置props选项
+//所以：此处需要借助于"编译器宏"函数接收子组件传递的数据
+// props 父传子
+const props=  defineProps({
+    car:String,
+    money:Number,
+ })
+ console.log(props.car)
+// 子传父
+ const emit = defineEmits(['changeMoney'])
+const changeMoney =()=>{
+    //触发changeMoney事件，传入参数1
+    emit('changeMoney',1)
+}
+</script>
+<template>
+<div class="son">
+        子组件---{{ car }}----{{ money }}
+        <button @click="changeMoney">花钱</button>
+    </div>
+</template>
+<style>
+   .son{
+        height: 70px;
+        width: 300px;
+        border:3px solid blue;
+        padding: 10px;
+        color: black;
+        background-color: wheat;
+    }
+</style>
