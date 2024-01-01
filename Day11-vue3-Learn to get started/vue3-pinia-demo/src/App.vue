e781f52d727f186619e82680357062c65c3fdf8b
ltpbje <19836788252@163.com>
2024-01-01 20:02:49 +0800

    157-Vue3-Pinia-storeToRefs方法完成

--- Day11-vue3-Learn to get started/vue3-pinia-demo/src/App.vue
+++ Day11-vue3-Learn to get started/vue3-pinia-demo/src/App.vue
@@ -3,12 +3,18 @@
   import son2Com from '@/components/son2-com.vue'
   import { userCounterStore } from '@/stores/counter.js' 
   import { getChannelStore } from '@/stores/channel.js'
+  import { storeToRefs } from 'pinia';
   const channelStore = getChannelStore() 
   const countStore =  userCounterStore()
+  //此时，直接解构，不处理，数据会丢失响应式
+  // const {count,msg} = countStore    
+  const {count,msg} = storeToRefs(countStore)
+  //作为action的increment可以直接解构
+   const { addCount }  = countStore 
 </script>
 
 <template>
-  <h1>我是父组件-{{ countStore.count }} -{{ countStore.msg }}   <button @click="countStore.addCount">+</button></h1>
+  <h1>我是父组件-{{ count }} -{{ msg }}   <button @click="addCount">+</button></h1>
   <son1Com></son1Com>
   <son2Com></son2Com>
   <button @click="channelStore.getChannelList">获取频道列表</button>
